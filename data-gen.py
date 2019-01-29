b9195097cec440d70d285c97cc9f31f7413f0946
Shreshth Tuli <shreshthtuli@gmail.com>
2019-01-29 16:22:23 +0530

    added clustered data generation

--- data-gen.py
+++ data-gen.py
@@ -2,10 +2,22 @@ import random
 
 filename = "points.dat"
 num_points = 1000
+k = 5
+clustered = 1
 
 f = open(filename, "w")
 
-for i in range(num_points):
-    f.write(str(random.randint(0, 100))+" "+str(random.randint(0, 100))+" "+str(random.randint(0, 100))+"\n")
+if clustered == 0:
+    for i in range(num_points):
+        f.write(str(random.randint(0, 100))+" "+str(random.randint(0, 100))+" "+str(random.randint(0, 100))+"\n")
+else:
+    for i in range(k):
+        x = random.randint(0, 100)
+        y = random.randint(0, 100)
+        z = random.randint(0, 100)
+        print x, y, z
+        for j in range(num_points / k):
+            f.write(str(x+random.randint(-20, 20))+" "+str(y+random.randint(-20, 20))+" "+str(z+random.randint(-20, 20))+"\n")
+
 
 f.close()
