c68e8b6ef0294a6c37fd8467dc4586940ce184d1
Shreshth Tuli <shreshthtuli@gmail.com>
2019-01-29 22:36:49 +0530

    updated graphs

--- results/plotter.m
+++ results/plotter.m
@@ -7,7 +7,8 @@ omp = [];
 
 x = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000];
 
-file1 = "5.txt";
+test = "50";
+file1 = strcat(test,".txt");
 f1 = fopen(file1, 'rt');
 
 fgetl(f1);
@@ -43,17 +44,30 @@ for i = 1:10
 end
    
 fgetl(f1);
+% 
+% pthread2_2 = seq ./ pthread2_2;
+% pthread2_4 = seq ./ pthread2_4;
+% pthread_2 = seq ./ pthread_2;
+% pthread_4 = seq ./pthread_4;
+% omp = seq ./omp;
 
-pthread2_2 = seq ./ pthread2_2;
-pthread2_4 = seq ./ pthread2_4;
-pthread_2 = seq ./ pthread_2;
-pthread_4 = seq ./pthread_4;
-omp = seq ./omp;
-
+h = figure;
 hold on;
-% plot(x, seq, '-o');
+grid on 
+%grid minor
+set(h,'Units','Inches');
+set(gca, 'YScale', 'log')
+% set(gca,'ytick',logspace(0,1,100000)-1);
+plot(x, seq, '-o');
 plot(x, pthread_2, '-o');
 plot(x, pthread_4, '-o');
 plot(x, pthread2_2, '-o');
 plot(x, pthread2_4, '-o');
+plot(x, omp, '-o');
+title(strcat("Performance comparison for k = ", test));
+xlabel("Number of Points")
+ylabel("Time in seconds")
 legend('Seq', 'pThread - 2', 'pThread - 4', 'pThread2 - 2', 'pThread2 - 4', 'omp')
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(h,strcat("k",test),'-dpdf','-r0')
